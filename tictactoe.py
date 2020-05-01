001281226843e93992361a2961bc912d16a1cfba
aadhar218 <aadharsharma218@gmail.com>
2020-05-01 15:40:47 +0530

    code updated

--- tictactoe.py
+++ tictactoe.py
@@ -2,6 +2,8 @@ import numpy as np
 a=""
 b=[]
 f=0
+b=0
+v=0
 print("Do you want to play tic-tac-toe")
 print("Press S to play single-player and M to play multi-player ")
 w,h=2,2
@@ -12,15 +14,19 @@ def game():
 		for j in range(3):
 			print(s[i][j],end=" ")
 		print()
-	
+
+
+		
 a=input()
 if (a=="M"):
+	k=0
+	f=0
 	c=0
 	d=0
 	game()
 	while(f==0):
         #code for multi-player
-		if(d==0):
+		if(d==0 ):
 			print("This the turn for player1 :")
 			print("Enter the location where you want to put a cross")
 			k=int(input())
@@ -35,8 +41,13 @@ if (a=="M"):
 						else:
 							s[i][j]='x'
 							c=0
+							if(s[0][0]=='x' and s[0][1]=='x' and s[0][2]=='x') or (s[1][0]=='x' and s[1][1]=='x' and s[1][2]=='x') or (s[2][0]=='x' and s[2][1]=='x' and s[2][2]=='x') or (s[0][0]=='x' and s[1][1]=='x' and s[2][2]=='x') or (s[0][0]=='x' and s[1][0]=='x' and s[2][0]=='x') or (s[0][1]=='x' and  s[1][1]=='x' and s[2][1]=='x') or (s[0][2]=='x' and  s[1][2]=='x' and s[2][2]=='x') or (s[2][0]=='x'and s[1][1]=='x' and s[0][2]=='x') :
+								b=1
+								f=1
 							game()
 							break
+			if(f==1):
+				break
 		if(c==0):
 			print("This the turn for player2 :")
 			print("Enter the location where you want to put a circle")
@@ -53,6 +64,19 @@ if (a=="M"):
 							s[i][j]='o'
 							d=0
 							game()
+							if(s[0][0]== s[0][1]==s[0][2]=='o') or (s[1][0]== s[1][1]== s[1][2]=='o') or (s[2][0]== s[2][1]==s[2][2]=='o') or (s[0][0]== s[1][1]== s[2][2]=='o') or (s[0][0]== s[1][0]==s[2][0]=='o') or (s[0][1]==  s[1][1]== s[2][1]=='o') or (s[0][2]==  s[1][2]== s[2][2]=='o') or (s[2][0]== s[1][1]==s[0][2]=='o') :
+								v=1
+								f=1
+							else:
+								f=0
 							break
-	
+			if(f==1):
+				break
+
+	if(b==1):
+		print("player1 wins")
+	elif(v==1):
+		print("player2 wins")
 		
+
+
